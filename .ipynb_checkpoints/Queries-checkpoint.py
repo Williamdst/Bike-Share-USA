82810dd4eba9e48611ab99b39ec854ec968840c5
Denzel Williams <schdwilliams@gmail.com>
2021-01-12 01:20:12 +0000

    Determined What Speed Outliers Are
    Figured out what speed outliers are. The next step is to update the trip table with distances, calculate speed, and then remove the outliers

--- .ipynb_checkpoints/Queries-checkpoint.py
+++ .ipynb_checkpoints/Queries-checkpoint.py
@@ -1,4 +1,6 @@
-def countYearlyTrips(conn):   # Query-0001 (Analyze)
+import pandas as pd
+
+def countYearlyTrips(conn) -> pd.DataFrame():   # Query-0001 (Analyze)
     cursor = conn.cursor()
     cursor.execute("rollback")
   
@@ -12,8 +14,9 @@ def countYearlyTrips(conn):   # Query-0001 (Analyze)
     
     colnames = [desc[0] for desc in cursor.description]
     data = cursor.fetchall()
- 
-    return (colnames, data)
+    
+    df = pd.DataFrame(data,columns=colnames)
+    return df
 
 "============================================================================="
 
@@ -47,7 +50,8 @@ def countYearlyNJTrips(conn): # Query-0002 (Analyze)
     colnames = [desc[0] for desc in cursor.description]
     data = cursor.fetchall()
     
-    return (colnames, data)
+    df = pd.DataFrame(data, columns=colnames)
+    return df
 
 "============================================================================="
 
@@ -70,7 +74,7 @@ def deleteNJTrips(conn) -> None: #(Tables)
                 """
     
     cursor.execute(deleteNJTrips_query)
-    
+    conn.commit()
     return None
 
 "============================================================================="
@@ -100,19 +104,37 @@ def create_foreign_key(conn, fk_table, fk_col, ref_table, ref_col, fk_name=''): 
 
 "============================================================================="
 
-def get_random_rows(conn, n_100K_rows: int=1) -> pd.DataFrame():
+def get_random_100k_rows(conn, shuffles: int=1, distance: bool=False) -> pd.DataFrame():
     """
     Randomly Samples 1% of data (1M rows) shuffles it and then takes the first n 100K rows
     """
-    # 10s * n_100k_rows = time to execute
+    # 10s * shuffles = time to execute
     
     cursor = conn.cursor()
     cursor.execute('rollback;')
     
+    get_row_query = ""
     row_data = pd.DataFrame()
-    get_row_query = """SELECT * FROM trip TABLESAMPLE system(1) order by random() limit 100000"""
     
-    for i in range(n_100K_rows):
+    if distance:
+        get_row_query = """
+                SELECT tp.*, round(CAST(ST_Distance(s1.geometry, s2.geometry)*0.000621371 AS numeric),2) AS distance
+                  FROM trip AS tp TABLESAMPLE SYSTEM(1) 
+                       LEFT JOIN station AS s1 
+                            ON tp.startid = s1.stationid
+                       LEFT JOIN station AS s2
+                            ON tp.endid = s2.stationid
+                 ORDER BY RANDOM()
+                 LIMIT 100000;
+                """
+    else:
+        get_row_query = """
+                SELECT * 
+                  FROM trip TABLESAMPLE SYSTEM(1) 
+                 ORDER BY RANDOM() LIMIT 100000;
+                """
+    
+    for i in range(shuffles):
         cursor.execute(get_row_query)
         colnames = [desc[0] for desc in cursor.description]
         data = cursor.fetchall()
@@ -127,4 +149,55 @@ def VACUUM(conn):
     cursor.execute('rollback;')
     cursor.execute('VACUUM;')
     conn.commit()
+    return None
+
+"============================================================================="
+
+
+def delete_duration_outliers(conn) -> None: #(Tables)
+    cursor = conn.cursor()
+    cursor.execute("rollback")
+
+    delete_duration_query = """
+             DELETE FROM trip
+             WHERE tripduration > 96
+                """
+    
+    cursor.execute(delete_duration_outliers)
+    conn.commit()    
+    return None
+
+"============================================================================="
+
+
+def find_time_swaps(conn) -> pd.DataFrame():
+    cursor = conn.cursor()
+    cursor.execute('rollback;')
+    
+    find_swaps_query = """
+                SELECT * 
+                  FROM trip 
+                 WHERE starttime >= endtime;
+                """
+    cursor.execute(find_swaps_query)
+    colnames = [desc[0] for desc in cursor.description]
+    data = cursor.fetchall()
+    
+    df = pd.DataFrame(data, columns=colnames)
+    return(df)
+
+"============================================================================="
+
+
+def delete_time_swaps(conn) -> None: #(Tables)
+    cursor = conn.cursor()
+    cursor.execute("rollback")
+
+    delete_duration_query = """
+             DELETE FROM trip
+             WHERE starttime > endtime
+                """
+    
+    cursor.execute(delete_duration_outliers)
+    conn.commit()    
     return None
